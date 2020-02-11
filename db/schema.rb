f00276e22ded2fee98828f108d757da687796ec0
chrissmith20 <christopher.v.smith20@gmail.com>
2020-02-11 10:37:24 -0500

    user information

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,6 @@ ActiveRecord::Schema.define(version: 2020_02_11_142221) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "first", default: "", null: false
-    t.string "last", default: "", null: false
     t.string "username", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
