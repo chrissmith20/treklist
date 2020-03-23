ab0ac6eb97926c0918c91396599c98e0d22d2a78
chrissmith20 <christopher.v.smith20@gmail.com>
2020-03-23 17:20:05 -0400

    destination rspec fix

--- spec/models/destination_spec.rb
+++ spec/models/destination_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 describe Destination do
 
-  it { should have_valid(:title).when("a title") }
-  it { should_not have_valid(:title).when(nil, "") }
+  it { should have_valid(:location).when("a location") }
+  it { should_not have_valid(:location).when(nil, "") }
 
   it { should have_valid(:description).when("a description") }
   it { should_not have_valid(:description).when(nil, "") }
